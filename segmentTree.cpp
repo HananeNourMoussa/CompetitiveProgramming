9b54fa29fb685e2748f2b507f80c36999c6513a2
Hanane Nour Moussa <71970059+HananeNourMoussa@users.noreply.github.com>
2021-01-27 14:58:59 +0100

    :)

---
+++ segmentTree.cpp
@@ -0,0 +1,53 @@
+//Following is the implementation of a segment tree that is used to process range sum queries and update queries
+
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+int n = 7;
+int t[28];
+//the build function
+void build(int a[], int v, int tl, int tr) {
+    if (tl == tr) {
+        t[v] = a[tl];
+    } else {
+        int tm = (tl + tr) / 2;
+        build(a, v*2, tl, tm);
+        build(a, v*2+1, tm+1, tr);
+        t[v] = t[v*2] + t[v*2+1];
+    }
+}
+//the sum function
+int sum(int v, int tl, int tr, int l, int r) {
+    if (l > r)
+        return 0;
+    if (l == tl && r == tr) {
+        return t[v];
+    }
+    int tm = (tl + tr) / 2;
+    return sum(v*2, tl, tm, l, min(r, tm))
+           + sum(v*2+1, tm+1, tr, max(l, tm+1), r);
+}
+
+
+//the update function
+void update(int v, int tl, int tr, int pos, int new_val) {
+    if (tl == tr) {
+        t[v] = new_val;
+    } else {
+        int tm = (tl + tr) / 2;
+        if (pos <= tm)
+            update(v*2, tl, tm, pos, new_val);
+        else
+            update(v*2+1, tm+1, tr, pos, new_val);
+        t[v] = t[v*2] + t[v*2+1];
+    }
+}
+int main (void)
+{
+    int arr[] = {1,2,3,4,5,6,7};
+    //building the segment tree
+    build (arr, 1, 0, n-1);
+    cout << "Sum from 3 to 5(inclusive): "<< sum(1,0,n-1,3,5);
+    return 0;
+}
