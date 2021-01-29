66628e60658d48f6f166d1d572fac07ac9fdc004
Hanane Nour Moussa <71970059+HananeNourMoussa@users.noreply.github.com>
2021-01-29 11:55:05 +0100

    *

---
+++ longestIncreasingSubsequence.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <iostream>
+//DP solution to the longest increasing subsequence problem using tabulation
+using namespace std;
+int T[100];
+void LIS (int* A)
+{
+    for (int i  = 0; i < 13; i++)
+    {
+        T[i]  = 1;
+        for (int j = 0; j < i; j++)
+        {
+            if((A[j] < A[i]) && (T[i] < T[j]+1))
+            {
+                T[i] = T[j] + 1;
+            }
+        }
+    }
+}
+int main (void)
+{
+    memset(T, 0, sizeof(T));
+    int A[] = {7, 2, 1, 3, 8, 4, 9, 1, 2, 6, 5, 9, 3};
+    LIS(A);
+    int maxi = 0;
+    for (int i = 0; i<13; i++)
+    {
+            if (T[i] > maxi)
+                maxi = T[i];
+    }
+    cout << maxi;
+    return 0;
+}
